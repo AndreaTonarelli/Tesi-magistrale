d5d2c340f8cb3af94934be3024f1b328c2a4c578
AndreaTonarelli <94750946+AndreaTonarelli@users.noreply.github.com>
2022-11-10 16:32:26 +0100

    Add files via upload

--- Thesis simulations/Flow in a tube/Flow_in_a_tube_vectorial.py
+++ Thesis simulations/Flow in a tube/Flow_in_a_tube_vectorial.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from   matplotlib import cm
-from numba import njit
 
 ## -------------------------------------------------------------------------------------------- ##
 ##                                     PRE-PROCESSING                                           ##
@@ -10,11 +9,12 @@ from numba import njit
 # Data
 P = 101325.0    # Pa
 T = 25+273.15   # K
+rho = 1000.0    # kg/m^3
 
-Lx = 5.0        # m
+Lx = 1.0        # m
 Ly = 1.0        # m
-nx = 40
-ny = 20
+nx = 5
+ny = 5
 nu = 1e-4       # m^2/s  (diffusion coeff)
 u_in = 0.5      # m/s    (inlet velocity)
 tau = 5.0       # s      (simulation time)
@@ -26,15 +26,15 @@ ve = 0          # m/s
 vw = 0          # m/s
 
 # Parameters for SOR (Poisson eq)
-max_iterations = 1000000
+max_iterations = 100000
 beta = 1.5
-max_error = 1e-5
+max_error = 1e-4
 
 ## -------------------------------------------------------------------------------------------- ##
 ##                                     DATA PROCESSING                                          ##
 ## -------------------------------------------------------------------------------------------- ##
-if nx % 2 != 0 and ny % 2 != 0:
-    print('\nOnly even number of cells can be accepted (for graphical purposes only)')
+# if nx % 2 /= 0 || ny % 2 /= 0:
+#    print('Only even number of cells can be accepted (for graphical purposes only)')
 
 # Process the grid
 hx = Lx / nx
@@ -106,27 +106,28 @@ ut = u
 ##                                         FUNCTIONS                                            ##
 ## -------------------------------------------------------------------------------------------- ##
 # Poisson equation solver
-@njit
 def Pressure_Poisson(p, ut, vt, gamma, nx, ny, hx, hy, dt, beta, max_iterations, max_error):
 
-    for iter in range(1,max_iterations+1):
+    pn = np.empty_like(p)
 
-        for i in range(1,nx+1):
-            for j in range(1,ny+1):
+    for iter in range(max_iterations):
 
-                delta = (p[i+1, j] + p[i-1,j])*hy/hx + (p[i, j+1] + p[i, j-1])*hx/hy
-                S = (1/dt) * ( (ut[i, j] - ut[i-1, j])*hy + (vt[i, j] - vt[i, j-1])*hx )
-                p[i, j] = beta * gamma[i, j] * (delta - S) + (1-beta) * p[i, j]
-        
+        pn = np.copy(p)
+
+        delta = (pn[2:, 1:-1] + pn[:-2, 1:-1])*hy/hx + (pn[1:-1, 2:] + pn[1:-1, :-2])*hx/hy
+        S = (1/dt) * ( (ut[1:, 1:-1] - ut[:-1, 1:-1])*hy + (vt[1:-1, 1:] - vt[1:-1, :-1])*hx )
+        p[1:-1, 1:-1] = beta * gamma[1:-1, 1:-1] * (delta - S) + (1-beta) * pn[1:-1, 1:-1]
+
+        if iter == 100:
+            print('delta', np.transpose(delta))
+            print('S', np.transpose(S))
+            print('p', np.transpose(p))
+            print('ut', np.transpose(ut))
+            print('vt', np.transpose(vt))
+
         # Estimate the error
-        epsilon = 0.0
-        for i in range(1,nx+1):
-            for j in range(1,ny+1):
-                delta = (p[i+1, j] + p[i-1,j])*hy/hx + (p[i, j+1] + p[i, j-1])*hx/hy
-                S = (1/dt) * ( (ut[i, j] - ut[i-1, j])*hy + (vt[i, j] - vt[i, j-1])*hx )
-                epsilon = epsilon + np.absolute( p[i, j] - gamma[i, j] * (delta - S) )
-
-        epsilon = epsilon / (nx*ny)
+        epsilon = np.abs(p[1:-1, 1:-1] - gamma[1:-1, 1:-1] * (delta - S))
+        epsilon = np.sum(epsilon) / (nx*ny)
 
         # Check the error
         if epsilon <= max_error:
@@ -135,15 +136,21 @@ def Pressure_Poisson(p, ut, vt, gamma, nx, ny, hx, hy, dt, beta, max_iterations,
     return p , iter
 
 # Advection diffusion equation
-@njit
 def AdvectionDiffusion2D(ut, vt, u, v, nx, ny, hx, hy, dt, nu, method):
 
     # Temporary u-velocity
-    for i in range(1, nx):
-        for j in range(1, ny+1):
+    # Centered Differencing Scheme
+    if method == 'CDS':
+        ue2 = ((u[2:, 1:-1] + u[1:-1, 1:-1]) / 2)**2 * hy
+        uw2 = ((u[1:-1, 1:-1] + u[:-2, 1:-1]) / 2)**2 * hy
+        unv = ((u[1:-1, 2:] + u[1:-1, 1:-1]) / 2) * ((v[1:-2, 1:] + v[2:-1, 1:]) / 2) * hx
+        usv = ((u[1:-1, 1:-1] + u[1:-1, :-2]) / 2) * ((v[1:-2, :-1] + v[2:-1, :-1]) / 2) * hx
+
+    if method == 'Upwind':         # DA RIGUARDARE PER TOGLIERE I CICLI FOR!!!!!!!!
+        for i in range(1, nx):
+            for j in range(1, ny+1):
 
             # Upwind
-            if method == 'Upwind':
                 flux_e = (u[i+1,j] + u[i,j]) / 2
                 flux_w = (u[i,j] + u[i-1,j]) / 2
                 flux_n = (v[i,j] + v[i+1,j]) / 2
@@ -163,31 +170,31 @@ def AdvectionDiffusion2D(ut, vt, u, v, nx, ny, hx, hy, dt, nu, method):
                 unv = flux_n * ufn * hx
                 usv = flux_s * ufs * hx
 
-            # Centered Differencing Scheme
-            if method == 'CDS':
-                ue2 = ((u[i+1, j] + u[i, j]) / 2)**2 * hy
-                uw2 = ((u[i, j] + u[i-1, j]) / 2)**2 * hy
-                unv = ((u[i, j+1] + u[i, j]) / 2) * ((v[i, j] + v[i+1, j]) / 2) * hx
-                usv = ((u[i, j] + u[i, j-1]) / 2) * ((v[i, j-1] + v[i+1, j-1]) / 2) * hx
+    H = hx*hy
+    A = (ue2 - uw2 + unv - usv) / H
 
-            H = hx*hy
-            A = (ue2 - uw2 + unv - usv) / H
+    De = nu * (u[2:, 1:-1] - u[1:-1, 1:-1]) * hy/hx
+    Dw = nu * (u[1:-1, 1:-1] - u[:-2, 1:-1]) * hy/hx
+    Dn = nu * (u[1:-1, 2:] - u[1:-1, 1:-1]) * hx/hy
+    Ds = nu * (u[1:-1, 1:-1] - u[1:-1, :-2]) * hx/hy
 
-            De = nu * (u[i+1, j] - u[i, j]) * hy/hx
-            Dw = nu * (u[i, j] - u[i-1, j]) * hy/hx
-            Dn = nu * (u[i, j+1] - u[i, j]) * hx/hy
-            Ds = nu * (u[i, j] - u[i, j-1]) * hx/hy
+    D = (De - Dw + Dn - Ds) / H
 
-            D = (De - Dw + Dn - Ds) / H
-
-            ut[i, j] = u[i, j] + dt*(-A + D)
+    ut[1:-1, 1:-1] = u[1:-1, 1:-1] + dt*(-A + D)
 
     # Temporary v-velocity
-    for i in range(1, nx+1):
-        for j in range(1, ny):
+    # Centered Differencing Scheme
+    if method == 'CDS':
+        vn2 = ((v[1:-1, 2:] + v[1:-1, 1:-1]) / 2)**2 * hx
+        vs2 = ((v[1:-1, 1:-1] + v[1:-1, :-2]) / 2)**2 * hx
+        veu = ((v[2:, 1:-1] + v[1:-1, 1:-1]) / 2) * ((u[1:, 1:-2] + u[1:, 2:-1]) / 2) * hy
+        vwu = ((v[1:-1, 1:-1] + v[:-2, 1:-1]) / 2) * ((u[:-1, 1:-2] + u[:-1, 2:-1]) / 2) * hy
+
+    if method == 'Upwind':         # DA RIGUARDARE PER TOGLIERE I CICLI FOR!!!!!!
+        for i in range(1, nx+1):
+            for j in range(1, ny):
 
             # Upwind
-            if method == 'Upwind':
                 flux_e = (u[i,j+1] + u[i,j]) / 2
                 flux_w = (u[i-1,j+1] + u[i-1,j]) / 2
                 flux_n = (v[i,j+1] + v[i,j]) / 2
@@ -207,24 +214,17 @@ def AdvectionDiffusion2D(ut, vt, u, v, nx, ny, hx, hy, dt, nu, method):
                 veu = flux_e * vfe * hy
                 vwu = flux_w * vfw * hy
 
-            # Centered Differencing Scheme
-            if method == 'CDS':
-                vn2 = ((v[i, j+1] + v[i, j]) / 2)**2 * hx
-                vs2 = ((v[i, j] + v[i, j-1]) / 2)**2 * hx
-                veu = ((v[i+1, j] + v[i, j]) / 2) * ((u[i, j] + u[i, j+1]) / 2) * hy
-                vwu = ((v[i, j] + v[i-1, j]) / 2) * ((u[i-1, j] + u[i-1, j+1]) / 2) * hy
+    H = hx*hy
+    A = (vn2 - vs2 + veu - vwu) / H
 
-            H = hx*hy
-            A = (vn2 - vs2 + veu - vwu) / H
+    De = nu * (v[2:, 1:-1] - v[1:-1, 1:-1]) * hy/hx
+    Dw = nu * (v[1:-1, 1:-1] - v[:-2, 1:-1]) * hy/hx
+    Dn = nu * (v[1:-1, 2:] - v[1:-1, 1:-1]) * hx/hy
+    Ds = nu * (v[1:-1, 1:-1] - v[1:-1, :-2]) * hx/hy
 
-            De = nu * (v[i+1, j] - v[i, j]) * hy/hx
-            Dw = nu * (v[i, j] - v[i-1, j]) * hy/hx
-            Dn = nu * (v[i, j+1] - v[i, j]) * hx/hy
-            Ds = nu * (v[i, j] - v[i, j-1]) * hx/hy
+    D = (De - Dw + Dn - Ds) / H
 
-            D = (De - Dw + Dn - Ds) / H
-
-            vt[i, j] = v[i, j] + dt*(-A + D)
+    vt[1:-1, 1:-1] = v[1:-1, 1:-1] + dt*(-A + D)
 
     return ut, vt
 
@@ -248,7 +248,7 @@ for it in range(1, nsteps+1):
     v[-1, nout_start-1:nout_end] = v[-2, nout_start-1:nout_end]   # zero-gradient outlet velocity
 
     # Advection-diffusion equation (predictor)
-    ut, vt = AdvectionDiffusion2D(ut, vt, u, v, nx, ny, hx, hy, dt, nu, 'Upwind')
+    ut, vt = AdvectionDiffusion2D(ut, vt, u, v, nx, ny, hx, hy, dt, nu, 'CDS')
 
     # Update boundary conditions for temporary velocities
     ut[0, nin_start-1:nin_end] = u_in       # fixed inlet velocity
@@ -263,7 +263,7 @@ for it in range(1, nsteps+1):
     v[1:nx+1, 1:ny] = vt[1:nx+1, 1:ny] - dt/hy * (p[1:nx+1, 2:ny+1] - p[1:nx+1, 1:ny])
 
     # Print on the screen
-    if it % 50 == 1:
+    if it % 10 == 0:
         print('Step:', it, '- Time:', t,  '- Poisson iterations:', iter)
 
     # Advance in time
@@ -277,27 +277,13 @@ uu[:,:] = (u[:, 1:] + u[:, :-1]) / 2
 vv[:,:] = (v[1:, :] + v[:-1, :]) /2
 pp[:,:] = (p[:-1, :-1] + p[:-1, 1:] + p[1:, :-1] + p[1:, 1:]) / 4
 
-# Creating a grid
-xx,yy = np.meshgrid(x,y)
-
-# Plotting the results
 fig = plt.figure(figsize=[1,1],dpi=400)
-plt1 = plt.contourf(xx, yy, np.transpose(pp))
-plt.colorbar(plt1)
-plt.title('Pressure')
+plt.contourf(np.transpose(pp))
 
 fig = plt.figure(figsize=[1,1],dpi=400)
-plt2 = plt.contourf(xx, yy, np.transpose(uu))
-plt.colorbar(plt2)
-plt.title('u - velocity')
+plt.contourf(np.transpose(uu))
 
 fig = plt.figure(figsize=[1,1],dpi=400)
-plt3 = plt.contourf(xx, yy, np.transpose(vv))
-plt.colorbar(plt3)
-plt.title('v - velocity')
-
-fig = plt.figure(figsize = (11,7), dpi=100)
-plt.quiver(xx, yy, np.transpose(uu), np.transpose(vv))
-plt.show()
+plt.contourf(np.transpose(vv))
 
 plt.show()
